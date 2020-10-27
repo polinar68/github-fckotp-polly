d7f1e5e4608873d1ce4afd49e6021b5a6398d7e6
polinar68 <lpollyramos68@gmail.com>
2020-10-27 17:37:25 +0800

    Initial commit

---
+++ src/app/components/components.ts
@@ -0,0 +1,401 @@
+export const Components = [
+  {
+    group: [
+      {
+        'name': 'Grids & Lists',
+        'href': 'grids_and_lists.html',
+        'header': true
+      },
+      {
+        'name': 'Data Grid',
+        'href': 'grid/grid.html',
+        'icon': 'view_column'
+      },
+      {
+        'name': 'Tree Grid',
+        'href': 'treegrid/tree_grid.html',
+        'icon': 'view_column'
+      },
+      {
+        'name': 'Hierarchical Grid',
+        'href': 'hierarchicalgrid/hierarchical_grid.html',
+        'icon': 'view_column'
+      },
+      {
+        'name': 'List View',
+        'href': 'list.html',
+        'icon': 'list'
+      },
+      {
+        'name': 'Combo',
+        'href': 'combo.html',
+        'icon': 'arrow_drop_down_circle'
+      },
+      {
+        'name': 'Spreadsheet',
+        'href': 'spreadsheet_overview.html',
+        'icon': 'apps'
+      },
+      {
+        'name': 'Charts',
+        'header': true
+      },
+      {
+        'name': 'Category Chart',
+        'href': 'categorychart.html',
+        'icon': 'bar_chart'
+      },
+      {
+        'name': 'Financial Chart',
+        'href': 'financial-chart.html',
+        'icon': 'trending_up'
+      },
+      {
+        'name': 'Doughnut Chart',
+        'href': 'doughnutchart.html',
+        'icon' : 'donut_large'
+      },
+      {
+        'name': 'Pie Chart',
+        'href': 'piechart.html',
+        'icon' : 'pie_chart'
+      },
+      {
+        'name': 'Sparkline',
+        'href': 'sparkline.html',
+        'icon' : 'show_chart'
+      },
+      {
+        'name': 'Maps',
+        'header': true
+      },
+      {
+        'name': 'Map',
+        'href': 'geo-map.html',
+        'icon': 'maps'
+      },
+      {
+        'name': 'Treemap',
+        'href': 'treemap-overview.html',
+        'icon': 'view_quilt'
+      },
+      {
+        'name': 'Gauges',
+        'header': true
+      },
+      {
+        'name': 'Bullet Graph',
+        'href': 'bulletgraph.html',
+        'icon': 'remove'
+      },
+      {
+        'name': 'Linear Gauge',
+        'href': 'lineargauge.html',
+        'icon': 'remove'
+      },
+      {
+        'name': 'Radial Gauge',
+        'href': 'radialgauge.html',
+        'icon': 'av_timer'
+      },
+      {
+        'name': 'Scheduling',
+        'header': true
+      },
+      {
+        'name': 'Calendar',
+        'href': 'calendar.html',
+        'icon': 'event'
+      },
+      {
+        'name': 'Date Time Editor',
+        'icon': 'access_time',
+        'href': 'date_time_editor.html'
+      },
+      {
+        'name': 'Date Picker',
+        'icon': 'date_range',
+        'href': 'date_picker.html'
+      },
+      {
+        'name': 'Month Picker',
+        'icon': 'date_range',
+        'href': 'month_picker.html'
+      },
+      {
+        'name': 'Time Picker',
+        'href': 'time_picker.html',
+        'icon': 'date_range'
+      },
+      {
+        'name': 'Date Range Picker',
+        'icon': 'date_range',
+        'href': 'date_range_picker.html'
+      }
+    ]
+  }, {
+    group: [
+      {
+        'name': 'Data Entry & Display',
+        'header': true
+      },
+      {
+        'name': 'Drop Down',
+        'href': 'drop_down.html',
+        'icon': 'view_list'
+      },
+      {
+        'name': 'Select',
+        'href': 'select.html',
+        'icon': 'view_list'
+      },
+      {
+        'name': 'Autocomplete',
+        'href': 'autocomplete.html',
+        'icon': 'view_list'
+      },
+      {
+        'name': 'Button',
+        'href': 'button.html',
+        'icon': 'crop_7_5',
+      },
+      {
+        'name': 'Button Group',
+        'href': 'buttonGroup.html',
+        'icon': 'group_work'
+      },
+      {
+        'name': 'Checkbox',
+        'href': 'checkbox.html',
+        'icon': 'check_box'
+      },
+      {
+        'name': 'Switch',
+        'href': 'switch.html',
+        'icon': 'toggle_on'
+      },
+      {
+        'name': 'Radio & Radio Group',
+        'href': 'radio_button.html',
+        'icon': 'radio_button_checked'
+      },
+      {
+        'name': 'Label & Input',
+        'href': 'label_input.html',
+        'icon': 'input'
+      },
+      {
+        'name': 'Input Group',
+        'href': 'input_group.html',
+        'icon': 'web'
+      },
+      {
+        'name': 'Mask',
+        'href': 'mask.html',
+        'icon': 'view_column'
+      },
+      {
+        'name': 'Badge',
+        'href': 'badge.html',
+        'icon': 'error'
+      },
+      {
+        'name': 'Icon',
+        'href': 'icon.html',
+        'icon': 'android'
+      },
+      {
+        'name': 'Divider',
+        'href': 'divider.html',
+        'icon': 'border_inner'
+      },
+      {
+        'name': 'Linear Progress',
+        'href': 'linear_progress.html',
+        'icon': 'poll'
+      },
+      {
+        'name': 'Circular Progress',
+        'href': 'circular_progress.html',
+        'icon': 'poll'
+      },
+      {
+        'name': 'Virtual For Directive',
+        'href': 'for_of.html',
+        'icon': 'view_column'
+      },
+      {
+        'name': 'Chip',
+        'href': 'chip.html',
+        'icon': 'edit_attributes'
+      },
+      {
+        'name': 'Text Highlight',
+        'href': 'texthighlight.html',
+        'icon': 'text_format'
+      },
+      {
+        'name': 'Frameworks',
+        'header': true
+      },
+      {
+        'name': 'Excel Library',
+        'href': 'excel_library.html',
+        'icon': 'table_chart'
+      },
+      {
+        'name': 'Services',
+        'header': true
+      },
+      {
+        'name': 'CSV Exporter',
+        'href': 'exporter_csv.html',
+        'icon': 'import_export'
+      },
+      {
+        'name': 'Excel Exporter',
+        'href': 'exporter_excel.html',
+        'icon': 'import_export'
+      },
+      {
+        'name': 'Transaction Service',
+        'href': 'transaction.html',
+        'icon': 'widgets'
+      }
+    ]
+  }, {
+    group: [
+      {
+        'name': 'Menus',
+        'header': true
+      },
+      {
+        'name': 'Navigation Drawer',
+        'href': 'navdrawer.html',
+        'icon': 'vertical_split'
+      },
+      {
+        'name': 'Navbar',
+        'href': 'navbar.html',
+        'icon': 'arrow_back'
+      },
+      {
+        'name': 'Layouts',
+        'header': true
+      },
+      {
+        'name': 'Layout Manager',
+        'href': 'layout.html',
+        'icon': 'view_quilt'
+      },
+      {
+        'name': 'Carousel',
+        'href': 'carousel.html',
+        'icon': 'view_carousel'
+      },
+      {
+        'name': 'Bottom Navigation',
+        'href': 'tabbar.html',
+        'icon': 'call_to_action'
+      },
+      {
+        'name': 'Tabs',
+        'href': 'tabs.html',
+        'icon': 'tab'
+      },
+      {
+        'name': 'Card',
+        'href': 'card.html',
+        'icon': 'crop_original'
+      },
+      {
+        'name': 'Avatar',
+        'href': 'avatar.html',
+        'icon': 'account_circle'
+      },
+      {
+        'name': 'Expansion Panel',
+        'href': 'expansion_panel.html',
+        'icon': 'expand_more'
+      },
+      {
+        'name': 'Splitter',
+        'href': 'splitter.html',
+        'icon': 'settings_ethernet'
+      },
+      {
+        'name': 'Dock Manager',
+        'href': 'dock-manager.html',
+        'icon': 'view_quilt'
+      },
+      {
+        'name': 'Interactions',
+        'header': true
+      },
+      {
+        'name': 'Dialog Window',
+        'href': 'dialog.html',
+        'icon': 'all_out'
+      },
+      {
+        'name': 'Slider',
+        'href': 'slider.html',
+        'icon': 'linear_scale'
+      },
+      {
+        'name': 'Ripple',
+        'href': 'ripple.html',
+        'icon': 'wifi_tethering'
+      },
+      {
+        'name': 'Toggle',
+        'href': 'toggle.html',
+        'icon': 'toggle_off'
+      },
+      {
+        'name': 'Tooltip',
+        'href': 'tooltip.html',
+        'icon': 'info'
+      },
+      {
+        'name': 'Overlay',
+        'href': 'overlay_main.html',
+        'icon': 'flip_to_front'
+      },
+      {
+        'name': 'Drag and Drop',
+        'href': 'drag_drop.html',
+        'icon': 'view_column'
+      },
+      {
+        'name': 'Zoom Slider',
+        'href': 'zoomslider-overview.html',
+        'icon': 'settings_ethernet'
+      },
+      {
+        'name': 'Action Strip',
+        'href': 'action-strip.html',
+        'icon': 'build_circle'
+      },
+      {
+        'name': 'Notifications',
+        'header': true
+      },
+      {
+        'name': 'Banner',
+        'href': 'banner.html',
+        'icon': 'view_stream'
+      },
+      {
+        'name': 'Snackbar',
+        'href': 'snackbar.html',
+        'icon': 'report'
+      },
+      {
+        'name': 'Toast',
+        'href': 'toast.html',
+        'icon': 'info'
+      }
+    ]
+  }
+];
